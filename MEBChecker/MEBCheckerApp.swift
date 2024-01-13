6c3dfd3335aadb2f9f5d312515400d11c8c46421
Tolnai Zoltan <tolnaiz@gmail.com>
2024-01-13 18:43:05 +0100

    Initial Commit

---
+++ MEBChecker/MEBCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEBCheckerApp.swift
+//  MEBChecker
+//
+//  Created by Tolnai Zoltán on 13/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct MEBCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
